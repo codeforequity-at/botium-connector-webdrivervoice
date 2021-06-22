157ad5b038ee43c53f6319c8e157e335777d45f4
Florian Treml <florian.treml@botium.at>
2021-06-22 19:44:40 +0200

    Added choice lists

--- index.js
+++ index.js
@@ -12,10 +12,50 @@ module.exports = {
     },
     capabilities: [
       {
-        name: 'WEBDRIVERVOICE_OPTIONS',
-        label: 'Webdriver.io Options',
-        type: 'json',
-        required: true
+        name: 'WEBDRIVERVOICE_GENDER',
+        label: 'Input Voice Gender',
+        type: 'choice',
+        required: false,
+        default: 'female',
+        choices: [
+          { name: 'female', key: 'female' },
+          { name: 'male', key: 'male' }
+        ]
+      },
+      {
+        name: 'WEBDRIVERVOICE_LANGUAGE',
+        label: 'Input Voice Language',
+        type: 'choice',
+        required: false,
+        default: 'us-english',
+        choices: [
+          { name: 'US English', key: 'us-english' },
+          { name: 'UK English', key: 'uk-english' },
+          { name: 'Spanish', key: 'es-spanish' },
+          { name: 'Japanese', key: 'japanese' },
+          { name: 'French', key: 'french' },
+          { name: 'German', key: 'german' },
+          { name: 'Portuguese', key: 'portuguese' },
+          { name: 'Italian', key: 'italian' }
+        ]
+      },
+      {
+        name: 'WEBDRIVERVOICE_OUTPUT_LANGUAGE',
+        label: 'Chatbot Voice Language',
+        type: 'choice',
+        required: false,
+        default: 'us-english',
+        choices: [
+          { name: 'US English', key: 'us-english' },
+          { name: 'UK English', key: 'uk-english' },
+          { name: 'Spanish', key: 'es-spanish' },
+          { name: 'Japanese', key: 'japanese' },
+          { name: 'French', key: 'french' },
+          { name: 'Chinese', key: 'chinese' },
+          { name: 'Portuguese', key: 'portuguese' },
+          { name: 'Arabic', key: 'arabic' },
+          { name: 'Hebrew-Israel', key: 'hebrew' }
+        ]
       }
     ]
   }
